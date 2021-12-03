d9c45b37813e7a7404b8d69c1c1401162798da71
akashtech-source <mr.ifranakashe.com>
2021-12-04 02:36:44 +0600

    dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Cart from "./components/Cart";
+import Home from "./components/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
